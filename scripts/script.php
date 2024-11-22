2b6f69112c26ea9249e89607a118670eb238ac39
GabrielHo9 <gabrielpereiraho@gmail.com>
2024-11-22 10:09:11 -0300

    Att Banco de dados
    Adicionado login no banco de dados, falta finalizar

--- scripts/script.php
+++ scripts/script.php
@@ -3,6 +3,7 @@ $conexao = mysqli_connect("localhost", "root", "", "");
 if (!$conexao) {
     die("Erro :( " . mysqli_connect_error());
 }
+
 /*
     ECHO NAO NECESSARIO, CODIGO APENAS PARA PROCESSAMENTO DE DADOS, DEVE ENVIAR O CLIENTE PARA A PRÓXIMA PÁGINA ANTES DE APARECER
 */
@@ -13,16 +14,27 @@ if (mysqli_query($conexao, "CREATE DATABASE IF NOT EXISTS banquinho")) {
         die("Falha na conexão com o banco de dados: " . mysqli_connect_error());
     }
 
-$sql = "
-    CREATE TABLE IF NOT EXISTS dados (
+
+$login = "
+    CREATE TABLE IF NO EXISTS cadastro (
         id INT NOT NULL AUTO_INCREMENT,
         nome VARCHAR(255),
         email VARCHAR(255),
-        reclamation VARCHAR(255),
-        opcao INT,
+        cpf VACHAR(255),
         PRIMARY KEY (id)
     )
 ";
+
+$sql = "
+    CREATE TABLE IF NOT EXISTS dados (
+        id INT NOT NULL AUTO_INCREMENT,
+        reclamation VARCHAR(255),
+        opcao INT,
+        situacao INT,
+        PRIMARY KEY (id)
+    )
+";
+
 if (!mysqli_query($conexao, $sql)) {
     die("Erro ao criar tabela: " . mysqli_error($conexao));
 }
@@ -30,19 +42,27 @@ if (!mysqli_query($conexao, $sql)) {
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = $_POST["name"];
     $email = $_POST["email"];
+    $cpf = $_POST["cpf"];
+    
     $reclamation = $_POST["reclamation"];
     $opcao = (int) $_POST["opcao"];
+    $situacao = (int) $_POST["situacao"];
 
-    $sql = "
-        INSERT INTO dados (nome, email, reclamation, opcao) 
-        VALUES ('$name', '$email', '$reclamation', $opcao)
+    $cadastro = "INSERT INTO dados (nome, email, cpf) 
+                 VALUES ('$name', '$email','$cpf')
     ";
+    $sql = "INSERT INTO dados (reclamation, opcao, situacao) 
+            VALUES ('$reclamation', '$opcao','$situacao')
+    ";
+}
+/* TESTE PARA SABER SE O CÓDIGO ESTÁ FUNCIONANDO CORRETAMENTE
     if (mysqli_query($conexao, $sql)) {
         echo "Dados inseridos com sucesso!";
     } else {
         echo "Erro :( " . mysqli_error($conexao);
     }
 }
+*/
 }
 mysqli_close($conexao);
 ?>
--- templts/index.php
+++ templts/index.php
@@ -8,27 +8,34 @@
     <script src="../scripts/script.js" defer></script>
 </head>
 <body>
+
+
     <div class="header" id="header">
         <button onclick="toggleSidebar()" class="btn_icon_header">
             <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
                 <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
             </svg>
         </button>
-        <div class="header-title">Ouvidoria Online</div>
+       
+        <p class="header-title">Ouvidoria Online</p>
+
         <div id="navigation_header" class="navigation_header">
             <button onclick="toggleSidebar()" class="btn_icon_header">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
                     <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z"/>
                 </svg>
             </button>
+
             <a href="#" class="active">Home</a>
             <a href="#">Login</a>
-            <a href="#">Sobre nós</a>
-            <a href="#">Contato</a>
+            <a href="#">Reclamaçõs</a>
+            <a href="#">Suas reclamações</a>
         </div>
+
         <div class="logo_header">
-            <img src="" class="img_logo_header" alt="Logo">
+            <img src="" class="img_logo_header,logo_header" alt="Logo">
         </div>
+
     </div>
 
     <fieldset>
@@ -40,14 +47,16 @@
             <input type="email" name="email" id="email" required>
             <label for="reclamation">Desktop</label>
             <textarea name="reclamation" id="reclamation" required></textarea>
-            <label for="oq">Ablubleble</label>
-            <select name="oq" id="opcao">
+            <label for="opcao">Ablubleble</label>
+            <select name="opcao" id="opcao">
                 <option value="1">Reclamation</option>
                 <option value="2">Validation</option>
                 <option value="3">Viadation</option>
                 <option value="4">Graduation</option>
             </select>
+            <br>
             <button type="submit">Botão :3</button>
+
         </form>
     </fieldset>
 </body>
